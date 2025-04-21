9398c4275d47a631e0b94fa7910976a6d6009835
SangamChaurasiya <sangam.chaurasiya0604@gmail.com>
2025-04-21 22:03:36 +0530

    account management

--- subplatform/writer/forms.py
+++ subplatform/writer/forms.py
@@ -1,9 +1,19 @@
 from django import forms
 from .models import Article
+from account.models import CustomUser
 
 
 class ArticleForm(forms.ModelForm):
     class Meta:
         model = Article
         fields = ['title', 'content', 'is_premium']
+
+
+class UpdateUserForm(forms.ModelForm):
+    password = None
+
+    class Meta:
+        model = CustomUser
+        fields = ['email', 'first_name', 'last_name']
+        exclude = ['password1', 'password2']
         
--- subplatform/writer/views.py
+++ subplatform/writer/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .forms import ArticleForm
+from .forms import ArticleForm, UpdateUserForm
 from django.http import HttpResponse
 from .models import Article
+from account.models import CustomUser
 
 
 @login_required(login_url='my-login')
@@ -80,4 +81,32 @@ def delete_article(request, pk):
 
     return render(request, 'writer/delete-article.html', context=context)
 
+
+@login_required(login_url='my-login')
+def account_management(request):
+    form = UpdateUserForm(instance=request.user)
+
+    if request.method == "POST":
+        form = UpdateUserForm(request.POST, instance=request.user)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('writer-dashboard')
+        
+    context = {'UserUpdateForm': form}
+        
+    return render(request, 'writer/account-management.html', context=context)
+
+
+@login_required(login_url='my-login')
+def delete_account(request):
+    if request.method == "POST":
+        deleteUser = CustomUser.objects.get(email=request.user)
+        deleteUser.delete()
+
+        return redirect('my-login')
     
+    return render(request, 'writer/delete-account.html')
+
+
